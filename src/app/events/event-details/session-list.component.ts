8046e08391431ab67df429a65d3291f569297258
unknown <USER-Admin@CTW-00164.criticaltechworks.com>
2018-10-24 17:48:56 +0100

    end of filters sorts and pipes

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -9,11 +9,13 @@ import { ISession } from "../shared";
 export class SessionListComponent implements OnChanges{
   @Input() sessions:ISession[]
   @Input() filterBy: string;
+  @Input() sortBy: string;
   visibleSessions: ISession[] = [];
 
   ngOnChanges(){
     if(this.sessions){
       this.filterSessions(this.filterBy);
+      this.sortBy === 'name' ? this.visibleSessions.sort(sortByNameAsc) : this.visibleSessions.sort(sortByVotesDesc)
     }
   }
 
@@ -22,7 +24,21 @@ export class SessionListComponent implements OnChanges{
       //creates a duplicate of the sessions
       this.visibleSessions = this.sessions.slice(0);
     } else {
-
+      this.visibleSessions = this.sessions.filter(session => {
+        return session.level.toLocaleLowerCase() === filter;
+      })
     }
   }
+
+
+}
+
+function sortByNameAsc(s1: ISession, s2:ISession){
+  if(s1.name > s2.name) return 1
+  if(s1.name === s2.name) return 0
+  else return -1
+}
+
+function sortByVotesDesc(s1:ISession, s2:ISession){
+  return s2.voters.length - s1.voters.length
 }
