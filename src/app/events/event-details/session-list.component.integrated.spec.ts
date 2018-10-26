c95b9537829a975f5141ae3ddfefda5fb8be888d
fidelsomething <filipe.teix@hotmail.com>
2018-10-26 09:38:03 +0100

    isolated and integrated tests

---
+++ src/app/events/event-details/session-list.component.integrated.spec.ts
@@ -0,0 +1,64 @@
+import {TestBed, async, ComponentFixture } from '@angular/core/testing'
+import { SessionListComponent } from './session-list.component';
+import { DebugElement } from '@angular/core';
+import { AuthService } from 'src/app/user/auth.service';
+import { VoterService } from './voter.service';
+import { UpvoteComponent } from './upvote.component';
+import { DurationPipe } from '../shared';
+import { CollapsibleWellComponent } from 'src/app/common';
+import { By } from '@angular/platform-browser';
+
+describe('SessionListComponent', () => {
+  let fixture : ComponentFixture<SessionListComponent>,
+  component : SessionListComponent,
+  element: HTMLElement,
+  debugEl: DebugElement
+
+  beforeEach(async(() => {
+    let mockAuthService =  {
+      isAuthenticated : () => true,
+      currentUser: {userName : 'Joe'}
+    }
+    let mockVoterService = {
+      userHasVoted : () => true
+    }
+
+    TestBed.configureTestingModule({
+      imports: [],
+      declarations: [
+        SessionListComponent,
+        UpvoteComponent,
+        DurationPipe,
+        CollapsibleWellComponent
+      ],
+      providers: [
+        { provide: AuthService, useValue: mockAuthService},
+        { provide: VoterService, useValue: mockVoterService}
+      ],
+      schemas: []
+    })
+  }))
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SessionListComponent)
+    component = fixture.componentInstance
+    debugEl = fixture.debugElement
+    element = fixture.nativeElement
+  })
+
+  describe('initial display', () =>{
+    it('should have the correct session', () => {
+      component.sessions = [{id:3, name: 'Session 1', presenter: 'Joe', duration:1, level: 'beginner', abstract: 'abstract', voters: ['john', 'bob']}]
+      component.filterBy = 'all'
+      component.sortBy = 'name'
+      component.eventId = 4
+
+      component.ngOnChanges()
+      fixture.detectChanges()
+
+      //expect(element.querySelector('[well-title]').textContent).toContain('Session 1')
+      expect(debugEl.query(By.css('[well-title]')).nativeElement.textContent).toContain('Session 1')
+
+    })
+  })
+})
